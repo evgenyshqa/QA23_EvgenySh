e8c59e1e5e1c4274c067cd72e82e246b8382a29a
evgenyshqa <evgenyshqa@users.noreply.github.com>
2020-04-25 19:49:52 +0300

    added test Login to Jira

---
+++ First_Selenium_Project/src/test/java/com/telran/qa/tests/LoginJira.java
@@ -0,0 +1,58 @@
+package com.telran.qa.tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class LoginJira {
+
+  WebDriver wd;
+
+
+  @BeforeClass
+  public void setUp() {
+    wd = new ChromeDriver();
+    wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    wd.get("http://jira.tel-ran.net/secure/Dashboard.jspa");
+
+  }
+
+  @Test
+  public void loginToJira() {
+    type(By.id("login-form-username"), "evgenyshqa");
+    type(By.id("login-form-password"), "111111");
+    click(By.id("login"));
+
+    Assert.assertTrue(isElementPresent(By.id("usernameerror")));
+
+    String errorMesage = wd.findElement(By.id("usernameerror")).getText();
+    Assert.assertEquals(errorMesage, "Sorry, your username and password are incorrect - please try again.");
+
+  }
+
+  public void type(By locator, String text) {
+    click(locator);
+    wd.findElement(locator).clear();
+    wd.findElement(locator).sendKeys(text);
+  }
+
+  public void click(By user) {
+    wd.findElement(user).click();
+  }
+
+  public boolean isElementPresent(By locator) {
+    return wd.findElements(locator).size() > 0;
+  }
+
+  @AfterClass
+  public void tearDown() {
+    wd.quit();
+
+  }
+}
